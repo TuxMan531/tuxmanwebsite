9cb14970b5b50f07ed1e2706ce22ee4ce462ca33
TuxMan531 <jaxf2005@gmail.com>
2024-11-14 18:29:32 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getBitcoinPrice() {
+    let price1;
+    let price2;
+    let amount1Worth
+    let diffrence1
+    // bit coin math
+    const response = await fetch('https://api.coindesk.com/v1/bpi/currentprice/BTC.json');
+    const data = await response.json();
+    price1 = data.bpi.USD.rate;
+    console.log("btc price: " + price1);
+    document.getElementById("btc").innerHTML = "$" + price1;
+    price1 = parseFloat(price1.replace(/,/g, ''));
+    // DOD coin math
+    const response2 = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd');
+    const data2 = await response2.json();
+    price2 = data2.dogecoin.usd;
+    console.log("doge price: " + price2);
+    document.getElementById("dodc").innerHTML = "$" + price2;
+    price2 = parseFloat(price2);
+
+    amount1Worth = 0.00016466 * price1;  // 0.00016466BTC for $15
+    amount2Worth = 49.5 * price2; // 49.5 DOGE for $20
+    diffrence1 = amount1Worth - 15;
+    diffrence2 = amount2Worth - 20;
+
+    document.getElementById("pl1").innerHTML = "$" + diffrence1.toFixed(3);
+    document.getElementById("worth1").innerHTML = "$" + amount1Worth.toFixed(3);
+    document.getElementById("worth2").innerHTML = "$" + amount2Worth.toFixed(3);
+    document.getElementById("pl2").innerHTML = "$" + diffrence2.toFixed(3);
+}
+
+async function startTracking() {
+    let couuntdown = 10;
+    while (true) {
+        await getBitcoinPrice();
+        while (couuntdown !== 0) {
+                console.log(couuntdown);
+                document.getElementById("update").innerHTML = couuntdown;
+                await sleep(1000); /// 1000 = 1 second
+                couuntdown = couuntdown - 1;
+            }
+            couuntdown = 10;
+        }
+    }
+
+startTracking();
+
+// what its worth rn, current price * bought amount = how much i have in btc rn
+// bought price * bought amount = how much i spent
